f05751b2b822a14943688b73fa419986c6f87826
SolomonSanjiv <solomonsanjiv.8888@gmail.com>
2025-04-10 23:07:22 +0530

    Update task3a1.py

--- CN/task3a1.py
+++ CN/task3a1.py
@@ -1,29 +1,33 @@
-def character_stuffing(data, start_delim, end_delim, escape_char):
-    stuffed_data = start_delim
+def character_stuffing(data, start, end, escape):
+    stuffed = start
     for char in data:
-        if char in [start_delim, end_delim, escape_char]:
-            stuffed_data += escape_char + char
+        if char in [start, end, escape]:
+            stuffed += escape + char
         else:
-            stuffed_data += char
-    stuffed_data += end_delim
-    return stuffed_data
-def character_destuffing(stuffed_data, start_delim, end_delim, escape_char):
-    destuffed_data = ""
+            stuffed += char
+    return stuffed + end
+
+def character_destuffing(stuffed, start, end, escape):
+    destuffed = ""
     i = 0
-    while i < len(stuffed_data):
-        if stuffed_data[i] == escape_char:
+    while i < len(stuffed):
+        if stuffed[i] == escape and i + 1 < len(stuffed):
+            destuffed += stuffed[i + 1]
+            i += 2
+        elif stuffed[i] not in [start, end]:
+            destuffed += stuffed[i]
             i += 1
-        elif stuffed_data[i] in [start_delim, end_delim]:
+        else:
             i += 1
-            continue
-        destuffed_data += stuffed_data[i]
-        i += 1
-    return destuffed_data
+    return destuffed
+
 data = "A B DLE D E DLE"
 start_delim = "DLESTX"
 end_delim = "DLEETX"
 escape_char = "DLE"
+
 stuffed = character_stuffing(data, start_delim, end_delim, escape_char)
-print("Stuffed Data:", stuffed)
+print(f"Stuffed Data: {stuffed}")
+
 destuffed = character_destuffing(stuffed, start_delim, end_delim, escape_char)
-print("Destuffed Data:", destuffed)
+print(f"Destuffed Data: {destuffed}")
