32136db2c54f9af71952ba6bb9f62d47318d5d46
SolomonSanjiv <solomonsanjiv.8888@gmail.com>
2025-04-10 22:49:04 +0530

    Update task1.py

--- CN/task1.py
+++ CN/task1.py
@@ -1,31 +1,20 @@
-def crc_remainder(input_bitstring, polynomial_bitstring, initial_filler):
-    polynomial_bitstring = polynomial_bitstring.lstrip("0")
-    len_input = len(input_bitstring)
-    initial_padding = (len(polynomial_bitstring) - 1) * initial_filler
-    input_padded_array = list(input_bitstring + initial_padding)
-    while "1" in input_padded_array[:len_input]:
-        cur_shift = input_padded_array.index("1")
-        for i in range(len(polynomial_bitstring)):
-            input_padded_array[cur_shift + i] = str(
-                int(polynomial_bitstring[i] != input_padded_array[cur_shift + i])
-            )
-    return "".join(input_padded_array)[len_input:]
-def crc_check(input_bitstring, polynomial_bitstring, check_value):
-    polynomial_bitstring = polynomial_bitstring.lstrip("0")
-    len_input = len(input_bitstring)
-    initial_padding = check_value
-    input_padded_array = list(input_bitstring + initial_padding)
-    while "1" in input_padded_array[:len_input]:
-        cur_shift = input_padded_array.index("1")
-        for i in range(len(polynomial_bitstring)):
-            input_padded_array[cur_shift + i] = str(
-                int(polynomial_bitstring[i] != input_padded_array[cur_shift + i])
-            )
-    return "1" not in "".join(input_padded_array)[len_input:]
+def compute_crc(s, p, pad):
+    p = p.lstrip("0")
+    l = len(s)
+    bits = list(s + pad)
+    while "1" in bits[:l]:
+        i = bits.index("1")
+        for j in range(len(p)):
+            bits[i+j] = str(int(p[j] != bits[i+j]))
+    return "".join(bits)[l:]
+
+def crc_remainder(s, p, f):
+    return compute_crc(s, p, f * (len(p.lstrip("0"))-1))
+
+def crc_check(s, p, cv):
+    return "1" not in compute_crc(s, p, cv)
+
 input_bits = "11010011101100"
-polynomial_bits = "1011"
-initial_filler = "0"
-remainder = crc_remainder(input_bits, polynomial_bits, initial_filler)
-print("CRC Remainder:", remainder)
-check_result = crc_check(input_bits, polynomial_bits, remainder)
-print("CRC Check Passed:", check_result)
+poly = "1011"
+rem = crc_remainder(input_bits, poly, "0")
+print(f"CRC: {rem}, Valid: {crc_check(input_bits, poly, rem)}")
